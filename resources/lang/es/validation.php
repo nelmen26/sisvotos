21f0296d47d3bad02875485da4987ef7b1200e58
Carlos Santillan Espinoza <carlos.santesp@gmail.com>
2018-11-13 22:10:31 -0400

    Corrigiendo validacion cantidad de votos al registrar votos

--- resources/lang/es/validation.php
+++ resources/lang/es/validation.php
@@ -109,5 +109,7 @@ return [
     | of "email". This simply helps us make messages a little cleaner.
     |
     */
-    'attributes' => [],
+    'attributes' => [
+        'votos.*' =>'votos'
+    ],
 ];
